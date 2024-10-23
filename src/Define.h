1a5635824859ddd0bc387a80e631a44e36d4c1ab
Thierry <t.cousin@anjouautomation.fr>
2024-10-23 09:41:10 +0200

    v6.0.0 qt6.7.1

--- src/Define.h
+++ src/Define.h
@@ -9,7 +9,7 @@
 #define _D_PROJET_PATH_FILE "/tcn-system/" + cMake_projectName
 
 const QStringList C_exeptDir = { "/0_Build", "/0_build", "/1_archive", "/1_Archive" , "/Debug" , "/Release" , "/.git", "/.vscode", "/.cache" , "/.metadata" , "/Packages" };
-const QStringList C_exeptFile = { ".txt.user", ".pro.user", "Thumbs.db", ".tcn.mp4", ".gitignore" };
+const QStringList C_exeptFile = { ".txt.user", ".pro.user", "Thumbs.db", ".tcn.mp4" };
 
 const QStringList C_videoFile_ext = { "ts", "mkv", "webm", "mp4", "mpeg", "m4v", "avi", "wmv", "flv", "mov" };
 const QStringList C_cppFile_ext = { "cpp", "c", "h", "hpp" };
